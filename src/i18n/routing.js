22e56a005b76cea7fcf335058ecc7c6d19f3c24e
prismaplatform <101548244+prismaplatform@users.noreply.github.com>
2025-08-04 12:19:04 +0300

    defaultee laguage changed

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "hu", "ro", "de"],
+  locales: ["hu", "de"],
 
   // Used when no locale matches
   defaultLocale: "hu",
