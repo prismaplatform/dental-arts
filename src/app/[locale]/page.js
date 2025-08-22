343a72b10ff98948e452c1c7201d08fdaed2d304
prismaplatform <101548244+prismaplatform@users.noreply.github.com>
2025-08-22 12:39:14 +0300

    pagespeed test

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -16,25 +16,29 @@ import Landing from "@/components/Landing";
 // import SlidingImages from '@/components/SlidingImages';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-      const scrollPosition = window.scrollY;
-      setTimeout(() => {
-        setIsLoading(false);
-        document.body.style.cursor = "default";
-        if (scrollPosition === 0) {
-          window.scrollTo(0, 0);
-        }
-      }, 2000);
-    })();
-  }, []);
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   (async () => {
+  //     const LocomotiveScroll = (await import("locomotive-scroll")).default;
+  //     const locomotiveScroll = new LocomotiveScroll();
+  //     const scrollPosition = window.scrollY;
+  //     setTimeout(() => {
+  //       setIsLoading(false);
+  //       document.body.style.cursor = "default";
+  //       if (scrollPosition === 0) {
+  //         window.scrollTo(0, 0);
+  //       }
+  //     }, 2000);
+  //   })();
+  // }, []);
 
   return (
     <>
-<AnimatePresence mode='wait'> {isLoading && <Preloader />} </AnimatePresence> <Landing />
+      {/* <AnimatePresence mode="wait">
+        {" "}
+        {isLoading && <Preloader />}{" "}
+      </AnimatePresence> */}
+      <Landing />
       {/* csak akkor fusson animáció a Hero-ban, ha showPreloader == true volt */}
       <TestSection />
       <WhyChouseUs2 />
